999d56e10ab5a7803bdff78f4b228734205843ef
camargo1990 <159648236+camargo1990@users.noreply.github.com>
2025-05-19 09:45:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.createElement('p');
+    mensagem.textContent = 'Bem-vindo à minha página de educação e tecnologia!';
+    document.body.appendChild(mensagem);
+}
